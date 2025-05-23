563ad4838520b0ee54fa12c555e2d502f99b4ec5
DillonThornberry <54234906+DillonThornberry@users.noreply.github.com>
2025-05-23 00:26:33 -0600

    parse lss files

---
+++ parseLss.py
@@ -0,0 +1,64 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+import sys
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Usage: python parseLss.py <lss_file>")
+        sys.exit(1)
+
+    # Parse the XML file
+    tree = ET.parse(f"./lss/{sys.argv[1]}")  # Replace with your actual file path
+    root = tree.getroot()
+
+    attempts = root.findall('.//AttemptHistory/Attempt')
+    segments = root.findall('.//Segments/Segment')
+
+    attemptList = []
+
+    for attempt in attempts:
+        attDict = {}
+        attDict['id'] = attempt.get('id')
+        attDict['started'] = attempt.get('started')
+        attDict['ended'] = attempt.get('ended')
+        #attDict['realTime'] = attempt.get('RealTime') if attempt.get('RealTime') else None
+
+        # Get nested RealTime
+        realTime = attempt.find('RealTime')
+        if realTime is not None:
+            attDict['realTime'] = realTime.text
+        else:
+            attDict['realTime'] = None
+
+        attemptList.append(attDict)
+
+
+    df = pd.DataFrame(attemptList)
+    df.set_index('id', inplace=True)
+
+    # apeDf['started'] = pd.to_datetime(apeDf['started'], format='%m/%d/%YT%H:%M:%S.%fZ')
+    # apeDf['ended'] = pd.to_datetime(apeDf['ended'], format='%Y-%m-%dT%H:%M:%S.%fZ')
+    # apeDf['realTime'] = pd.to_timedelta(apeDf['realTime'])
+
+    for segment in segments:
+        name = segment.find('Name').text
+        
+        print(name)
+        df[name] = None
+
+        for segmentAttempt in segment.find('SegmentHistory').findall('Time'):
+            attId = segmentAttempt.get('id')
+            realTime = segmentAttempt.find('RealTime').text if segmentAttempt.find('RealTime') != None else None
+
+            df.at[attId, name] = realTime
+
+
+    print(df.head())
+    df.to_csv(f"./csv/{sys.argv[1].split('.')[0]}.csv", index=True)
+    print(f"Data saved to {sys.argv[1].split('.')[0]}.csv")
+
+
+if __name__ == "__main__":  
+    main()
